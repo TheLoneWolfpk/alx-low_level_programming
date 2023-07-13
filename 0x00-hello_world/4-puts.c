c4ffaa648995c3b9069253bf842b02137132fc30
Prince <prince.affum45@gmail.com>
2023-07-13 10:42:06 -0700

    learning C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - this is the main function
  *
  * int - return type
  *
  * Description : prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
- * /
+ */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle);
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
