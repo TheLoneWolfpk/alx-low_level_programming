97721b13588aa46359c38b30d0748095e797f13c
Prince <prince.affum45@gmail.com>
2023-07-20 10:41:42 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* print_diagonal - prints \ relative to n
+* @n: number of \ to be printed
+*
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+int a, b;
+
+if (n <= 0)
+{
+	_putchar('\n');
+}
+else
+{
+	for (a = 1; a <= n; a++)
+	{
+	for (b = 1; b < a; b++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+	}
+}
+return;
+}
