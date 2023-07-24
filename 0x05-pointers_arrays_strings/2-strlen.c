f5672c3dbc12f6533b3068aa96f9f6ddb5c0b996
Prince <prince.affum45@gmail.com>
2023-07-24 10:07:59 -0700

    P and A

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - gets length of a string
+ * @s: string
+ *
+ * Return: always len 
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
