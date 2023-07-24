59ef89d23c216ea4d0574e1b1cbb981b3c3729ab
Prince <prince.affum45@gmail.com>
2023-07-24 16:13:39 -0700

    P and A

--- 0x05-pointers_arrays_strings/.7-puts_half.c
+++ 0x05-pointers_arrays_strings/.7-puts_half.c
@@ -6,28 +6,12 @@
  */
 void puts_half(char *str)
 {
-	int j = 0;
+	int i, n, count = 0;
 
-	int k;
-
-	while (str[j] != '\0')
-	{
-		j++;
-	}
-
-	if (j % 2 == 1)
-	{
-		k = (j - 1) / 2;
-		k += 1;
-	}
-	else
-	{
-		k = j / 2;
-	}
-
-	for (; k < j; k++)
-	{
-		_putchar(str[k]);
-	}
+	for (i = 0 ; str[i] != '\0' ; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
