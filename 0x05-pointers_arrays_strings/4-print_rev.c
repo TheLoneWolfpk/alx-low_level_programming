76760aec1100bf35092cf41df75b834abcd69fa2
Prince <prince.affum45@gmail.com>
2023-07-24 13:45:49 -0700

    P and A

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: string pointer
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+	if (len != 0)
+	{
+		while (len > 0)
+		{
+			_putchar(*(s + (len - 1)));
+			len--;
+		}
+	}
+	_putchar('\n');
+
+	return;
+}
