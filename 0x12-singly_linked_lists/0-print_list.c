1c9161b18e5383c9c7534d3c9faf955cd231e726
TheLoneWolfpk <prince.affum45@gmail.com>
2023-08-24 06:51:22 -0700

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_list - print all the elements of a list
+ * print_list - prints all the elements of a list
  * @h: singly linked list
  * Return: number of elements in the list
  */
