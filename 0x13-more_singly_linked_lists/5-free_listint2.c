773b401a7442e0e08ea13a14d16459122f45ba1b
TheLoneWolfpk <prince.affum45@gmail.com>
2023-08-28 23:50:03 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,8 +3,7 @@
 #include "lists.h"
 /**
  * free_listint2 - Frees a listint_t list.
- * @head: a pointer to the address of the 
- * 	head of the listint_t.
+ * @head: a pointer to the address of the head of the listint_t.
  *
  * Description: sets the head to NULL
  */
